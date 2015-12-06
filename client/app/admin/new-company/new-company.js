6a2ed2ae1f971304450a3c09477d73f0bc013dd9
Matteo <mettiu@gmail.com>
2015-12-06 16:03:26 +0100

    alligned state definitions to new layout and ui-router structure

--- client/app/admin/new-company/new-company.js
+++ client/app/admin/new-company/new-company.js
@@ -1,18 +1,19 @@
-//'use strict';
-//
-//angular.module('planApp')
-//  .config(function ($stateProvider) {
-//    $stateProvider
-//      .state('frame.new-company', {
-//        url: '/new-company?companyId',
-//        views: {
-//          "main-content": {
-//            templateUrl: 'app/admin/new-company/new-company.html',
-//            controller: 'NewCompanyCtrl',
-//            controllerAs: 'NewCompanyCtrl',
-//            authenticate: true,
-//            resolve: EditCompanyResolver
-//          }
-//        }
-//      });
-//  });
+'use strict';
+
+angular.module('planApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('new-company', {
+        parent: 'master-app-layout-frame',
+        url: '/new-company',
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/admin/new-company/new-company.html',
+            controller: 'NewCompanyCtrl',
+            controllerAs: 'NewCompanyCtrl',
+            authenticate: true,
+            resolve: EditCompanyResolver
+          }
+        }
+      });
+  });

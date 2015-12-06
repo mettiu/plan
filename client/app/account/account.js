fda23322f9f618d3941b6fe6f1cdc7a93dde7886
Matteo <mettiu@gmail.com>
2015-12-06 16:11:45 +0100

    alligned state definitions to new layout and ui-router structure

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -1,22 +1,38 @@
 'use strict';
 
-//angular.module('planApp')
-//  .config(function ($stateProvider) {
-//    $stateProvider
-//      .state('login', {
-//        url: '/login',
-//        templateUrl: 'app/account/login/login.html',
-//        controller: 'LoginCtrl'
-//      })
-//      .state('signup', {
-//        url: '/signup',
-//        templateUrl: 'app/account/signup/signup.html',
-//        controller: 'SignupCtrl'
-//      })
-//      .state('settings', {
-//        url: '/settings',
-//        templateUrl: 'app/account/settings/settings.html',
-//        controller: 'SettingsCtrl',
-//        authenticate: true
-//      });
-//  });
+angular.module('planApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('login', {
+        parent: 'master-app-layout-frame',
+        url: '/login',
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/account/login/login.html',
+            controller: 'LoginCtrl'
+          }
+        }
+      })
+      .state('signup', {
+        parent: 'master-app-layout-frame',
+        url: '/signup',
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/account/signup/signup.html',
+            controller: 'SignupCtrl'
+          }
+        }
+      })
+      .state('settings', {
+        parent: 'master-app-layout-frame',
+        url: '/settings',
+        authenticate: true,
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/account/settings/settings.html',
+            controller: 'SettingsCtrl'
+          }
+        }
+      });
+  });
+

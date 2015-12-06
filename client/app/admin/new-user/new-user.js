bf84eb5298a7c7b9ea4e94329a289dbcc2bb045c
Matteo <mettiu@gmail.com>
2015-12-06 16:05:38 +0100

    alligned state definitions to new layout and ui-router structure

--- client/app/admin/new-user/new-user.js
+++ client/app/admin/new-user/new-user.js
@@ -1,18 +1,20 @@
-//'use strict';
-//
-//angular.module('planApp')
-//  .config(function ($stateProvider) {
-//    $stateProvider
-//      .state('frame.new-user', {
-//        url: '/new-user?userId',
-//        views: {
-//          "main-content": {
-//            templateUrl: 'app/admin/new-user/new-user.html',
-//            controller: 'NewUserCtrl',
-//            controllerAs: 'NewUserCtrl',
-//            authenticate: true,
-//            resolve: EditUserResolver
-//          }
-//        }
-//      });
-//  });
+'use strict';
+
+angular.module('planApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('new-user', {
+        parent: 'master-app-layout-frame',
+        url: '/new-user?userId',
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/admin/new-user/new-user.html',
+            controller: 'NewUserCtrl',
+            controllerAs: 'NewUserCtrl',
+            authenticate: true,
+            resolve: EditUserResolver
+          }
+        }
+      });
+  });
+

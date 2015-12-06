c5e0a4516433d14f84dda2770ebd6f4ae57b51ee
Matteo <mettiu@gmail.com>
2015-12-06 16:02:09 +0100

    alligned state definitions to new layout and ui-router structure

--- client/app/admin/features/features.js
+++ client/app/admin/features/features.js
@@ -1,18 +1,19 @@
-//'use strict';
-//
-//angular.module('planApp')
-//  .config(function ($stateProvider) {
-//    $stateProvider
-//      .state('frame.features', {
-//        url: '/features',
-//        views: {
-//          "main-content": {
-//            templateUrl: 'app/admin/features/features.html',
-//            controller: 'FeaturesCtrl',
-//            controllerAs: 'FeaturesCtrl',
-//            authenticate: true,
-//            resolve: FeaturesResolver
-//          }
-//        }
-//      });
-//  });
+'use strict';
+
+angular.module('planApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('features', {
+        parent: 'master-app-layout-frame',
+        url: '/features',
+        views: {
+          'main-content@master-app-layout': {
+            templateUrl: 'app/admin/features/features.html',
+            controller: 'FeaturesCtrl',
+            controllerAs: 'FeaturesCtrl',
+            authenticate: true,
+            resolve: FeaturesResolver
+          }
+        }
+      });
+  });

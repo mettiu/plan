970001671486604f8eaa15d2d73e14e1a7c46c0d
Matteo <mettiu@gmail.com>
2015-12-08 10:21:06 +0100

    user model test spec review

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -7,17 +7,13 @@ var app = require('../../app');
 var User = require('./user.model');
 var Company = require('../company/company.model');
 
-var companyArraySize = 3;
+var companyArraySize = 300;
 
 var user = new User({
   provider: 'local',
   name: 'Fake User',
   email: 'test@test.com',
   password: 'password'
-  //acl: [
-  //  {_company: '5665de97e664a6501af6f8f7', roles:['none', 'purchase']},
-  //  {_company: '5665de97e664a6501af6f8f8', roles:['admin', 'none']}
-  //]
 });
 
 var companyArray = [];
@@ -28,22 +24,14 @@ for (var i = 0; i < companyArraySize; i++) {
   });
   companyArray.push(company);
 }
-//console.log(companyArray);
-
-//var company = new Company({
-//  name: 'Test Company Inc.',
-//  info: faker.company.companyName() + ' ' + faker.company.companySuffix(),
-//  active: true
-//});
 
 describe('User Model with Company', function () {
   before(function (done) {
     // Clear users before testing
     User.remove().exec().then(function () {
-      done();
-    });
-    Company.remove().exec().then(function () {
-      done();
+      Company.remove().exec().then(function () {
+        done();
+      });
     });
   });
 
@@ -56,11 +44,11 @@ describe('User Model with Company', function () {
   it('should begin with no users and no companies', function (done) {
     User.find({}, function (err, users) {
       users.should.have.length(0);
+      Company.find({}, function (err, companies) {
+        companies.should.have.length(0);
+        done();
+      });
     });
-    Company.find({}, function (err, companies) {
-      companies.should.have.length(0);
-    });
-    done();
   });
 
   it('should save ' + companyArraySize + ' fake companies', function (done) {
@@ -70,7 +58,7 @@ describe('User Model with Company', function () {
     done();
   });
 
-  it('should find all the ' + companyArraySize + ' fake companies saved', function(done) {
+  it('should find all the ' + companyArraySize + ' fake companies saved', function (done) {
     Company.find({}, function (err, comps) {
       comps.should.have.length(companyArraySize);
     });
@@ -78,9 +66,9 @@ describe('User Model with Company', function () {
   });
 
   it('should save the fake user', function (done) {
-    user.acls =   [
-      {_company: companyArray[0]._id, role:'fakeRole-1' },
-      {_company: companyArray[1]._id, role:'fakeRole-2' }
+    user.acls = [
+      {_company: companyArray[0]._id, role: 'fakeRole-1'},
+      {_company: companyArray[1]._id, role: 'fakeRole-2'}
     ];
     user.save(function () {
       User.find({}, function (err, users) {
@@ -90,26 +78,26 @@ describe('User Model with Company', function () {
     });
   });
 
-  it('should add a new role to User', function(done) {
+  it('should add a new role to User', function (done) {
     var aclsCount = user.acls.length;
     user.setUserRoleForCompany('fakeRole-3', companyArray[0]._id).should.be.equal(true);
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount + 1);
       //console.log("dopo: " + acls);
       done();
     });
   });
 
-  it('should check role existence for User', function(done) {
+  it('should check role existence for User', function (done) {
     user.hasAuthorizationForRoleAndCompanyId('fakeRole-3', companyArray[0]._id).should.be.equal(true);
     user.hasAuthorizationForRoleAndCompanyId('non-sense', companyArray[0]._id).should.be.equal(false);
     done();
   });
 
-  it('should not add a duplicate role to User', function(done) {
+  it('should not add a duplicate role to User', function (done) {
     var aclsCount = user.acls.length;
     user.setUserRoleForCompany('fakeRole-3', companyArray[0]._id).should.be.equal(false);
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount);
       done();
     });
@@ -125,40 +113,40 @@ describe('User Model with Company', function () {
     done();
   });
 
-  it('should add an acl with a new role to User', function(done) {
+  it('should add an acl with a new role to User', function (done) {
     var aclsCount = user.acls.length;
     user.setUserAcl({role: 'fakeRole-4', _company: companyArray[0]._id}).should.be.equal(true);
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount + 1);
       done();
     });
   });
 
-  it('should not add an acl with a duplicate role to User', function(done) {
+  it('should not add an acl with a duplicate role to User', function (done) {
     var aclsCount = user.acls.length;
     user.setUserAcl({role: 'fakeRole-4', _company: companyArray[0]._id}).should.be.equal(false);
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount);
       done();
     });
   });
 
-  it('should not add a badly formatted acl', function(done) {
+  it('should not add a badly formatted acl', function (done) {
     var aclsCount = user.acls.length;
     user.setUserAcl({roleNonSense: 'fakeRole-4', _company: companyArray[0]._id}).should.be.equal(false);
     user.setUserAcl({role: 'fakeRole-4', _companyNonSense: companyArray[0]._id}).should.be.equal(false);
-    user.setUserAcl({ _company: companyArray[0]._id}).should.be.equal(false);
+    user.setUserAcl({_company: companyArray[0]._id}).should.be.equal(false);
     user.setUserAcl({roleNonSense: 'fakeRole-4'}).should.be.equal(false);
     user.setUserAcl({}).should.be.equal(false);
     user.setUserAcl(123456).should.be.equal(false);
     user.setUserAcl("123456").should.be.equal(false);
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount);
       done();
     });
   });
 
-  it('should add some acls to User', function(done) {
+  it('should add some acls to User', function (done) {
     var aclsCount = user.acls.length;
     user.setUserAcls([
       {role: 'fakeRole-1', _company: companyArray[0]._id}, // already exists
@@ -167,58 +155,36 @@ describe('User Model with Company', function () {
       {role: 'fakeRole-4', _company: companyArray[0]._id}, // already exists
       {role: 'fakeRole-5', _company: companyArray[0]._id}  // new
     ]).should.be.equal(2); // 2 inserted
-    user.save().then(function(savedUser) {
+    user.save().then(function (savedUser) {
       savedUser.acls.should.have.length(aclsCount + 2);
       done();
     });
   });
 
-});
+  it('should fail when saving a duplicate user', function (done) {
+    user.save(function () {
+      var userDup = new User(user);
+      userDup.save(function (err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
 
-//describe('User Model', function () {
-//  before(function (done) {
-//    // Clear users before testing
-//    User.remove().exec().then(function () {
-//      done();
-//    });
-//  });
-//
-//  afterEach(function (done) {
-//    User.remove().exec().then(function () {
-//      done();
-//    });
-//  });
-//
-//  it('should begin with no users', function (done) {
-//    User.find({}, function (err, users) {
-//      users.should.have.length(0);
-//      done();
-//    });
-//  });
-//
-//  it('should fail when saving a duplicate user', function (done) {
-//    user.save(function () {
-//      var userDup = new User(user);
-//      userDup.save(function (err) {
-//        should.exist(err);
-//        done();
-//      });
-//    });
-//  });
-//
-//  it('should fail when saving without an email', function (done) {
-//    user.email = '';
-//    user.save(function (err) {
-//      should.exist(err);
-//      done();
-//    });
-//  });
-//
-//  it("should authenticate user if password is valid", function () {
-//    return user.authenticate('password').should.be.true;
-//  });
-//
-//  it("should not authenticate user if password is invalid", function () {
-//    return user.authenticate('blah').should.not.be.true;
-//  });
-//});
+  it('should fail when saving without an email', function (done) {
+    user.email = '';
+    user.save(function (err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it("should authenticate user if password is valid", function () {
+    return user.authenticate('password').should.be.true;
+  });
+
+  it("should not authenticate user if password is invalid", function () {
+    return user.authenticate('blah').should.not.be.true;
+  });
+
+});

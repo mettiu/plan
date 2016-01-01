17749dc3c5d2d4de51d1ade08c6f8e58860b5f82
Matteo <mettiu@gmail.com>
2016-01-01 18:37:35 +0100

    removed createNew static method: create is already eredited from model

--- server/api/company/company.model.js
+++ server/api/company/company.model.js
@@ -4,23 +4,36 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CompanySchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  name: {
+    type: String,
+    minlength: 5,
+    maxlength: 100,
+    required: true
+  },
+  info: {
+    type: String,
+    minlength: 0,
+    maxlength: 100
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  purchaseUsers: [{
+  type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+}],
+  teamUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
+  adminUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }]
 });
-
-
-/**
- * Statics
- */
-CompanySchema
-  .statics = {
-
-  createNew: function (model, callback) {
-    (new this(model)).save(callback);
-  }
-
-};
-
 
 module.exports = mongoose.model('Company', CompanySchema);

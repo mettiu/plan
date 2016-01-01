37fb1b3b25d65d15288152015b6be168ac782091
Matteo <mettiu@gmail.com>
2016-01-01 18:39:17 +0100

    define test: - tests for model methods - tests for controller methods to achieve result, this test sets new routes using only the method as a express single middleware

--- server/api/company/company.spec.js
+++ server/api/company/company.spec.js
@@ -1,20 +1,121 @@
 'use strict';
 
-var should = require('should');
+var express = require('express');
+var chai = require('chai');
+var expect = chai.expect;
 var app = require('../../app');
+var companyController = require('./company.controller.js');
+var Company = require('./company.model');
+var utils = require('../../components/utils')
 var request = require('supertest');
 
-describe('GET /api/companies', function() {
+var company;
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/companies')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
+describe('Test Company creation', function () {
+
+  // remove all companies from DB
+  afterEach(function (done) {
+    utils.mongooseRemoveAll([Company], done);
   });
+
+  // set company variable with test data
+  before(function() {
+    company = {
+      name: "test company",
+      info: "test company info"
+    };
+  });
+
+  describe('Model test', function() {
+
+    describe('Test method: create', function() {
+      it('should create a company', function (done) {
+        Company.create(company, function (err, created) {
+          if (err) return done(err);
+          Company.count({}, function(err, count) {
+            if (err) return done(err);
+            expect(count).to.be.equal(1);
+            return done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('Controller test', function() {
+
+    // set the test route for this controller method
+    before(function () {
+      app.use('/test/company/create', express.Router().post('/', companyController.create));
+    });
+
+    describe('Test method: create', function () {
+
+      it('should create company', function (done) {
+        request(app)
+          .post('/test/company/create')
+          .send(company)
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .end(function (err, res) {
+            if (err) return done(err);
+            Company.count({}, function(err, count) {
+              if (err) return done(err);
+              expect(count).to.be.equal(1);
+              return done();
+            });
+          });
+      });
+
+    });
+
+  });
+
 });
+
+//describe('Test Company controller', function () {
+//
+//  var req = {};
+//  var res = {};
+//
+//
+//
+//
+//  after(function (done) {
+//    utils.mongooseRemoveAll([Company], done);
+//  });
+//
+//  describe('Test create company', function () {
+//
+//    req.body = company;
+//
+//    it('should create company', function (done) {
+//      request(app)
+//        .post('/api/company/test')
+//        .send(company)
+//        .expect(201)
+//        .expect('Content-Type', /json/)
+//        .end(function (err, res) {
+//          if (err) return done(err);
+//          done();
+//        });
+//    })
+//  });
+//
+//});
+
+
+//describe('GET /api/companies', function() {
+//
+//  it('should respond with JSON array', function(done) {
+//    request(app)
+//      .get('/api/companies')
+//      .expect(200)
+//      .expect('Content-Type', /json/)
+//      .end(function(err, res) {
+//        if (err) return done(err);
+//        res.body.should.be.instanceof(Array);
+//        done();
+//      });
+//  });
+//});

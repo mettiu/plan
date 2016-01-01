ab0eb2c3b09508920f068c4372da9a11533243c0
Matteo <mettiu@gmail.com>
2016-01-02 00:01:57 +0100

    Introduced active parameter in company controller's index method

--- server/api/company/company.controller.js
+++ server/api/company/company.controller.js
@@ -3,60 +3,97 @@
 var _ = require('lodash');
 var Company = require('./company.model');
 
-// Creates a new company in the DB.
-exports.create = function(req, res) {
-  Company.create(req.body, function(err, company) {
-    if(err) { return handleError(res, err); }
+/**
+ * Creates new companiy in DB.
+ * @param req
+ * @param res
+ */
+exports.create = function (req, res) {
+  Company.create(req.body, function (err, company) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(201).json(company);
   });
 };
 
-// Get list of companys
-exports.index = function(req, res) {
-  Company.find(function (err, companys) {
-    if(err) { return handleError(res, err); }
+/**
+ * List companies. Accepts optional parameter req.body.active <boolean> to match only active
+ * or inactive companies.
+ * @param req
+ * @param res
+ */
+exports.index = function (req, res) {
+  var query = {};
+  //console.log(req.body.active !== undefined && typeof(req.body.active) === "boolean");
+  if (req.body.active !== undefined && typeof(req.body.active) === "boolean") query = {active: req.body.active};
+  console.log(query);
+  Company.find(query, function (err, companys) {
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(200).json(companys);
   });
 };
 
 // Get list of companys
-exports.find = function(req, res) {
+exports.find = function (req, res) {
   Company.find({'name': new RegExp('^' + req.query.value, 'i')}, '_id name', function (err, companies) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(200).json(companies);
   });
 };
 
 // Get a single company
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Company.findById(req.params.id, function (err, company) {
-    if(err) { return handleError(res, err); }
-    if(!company) { return res.status(404).send('Not Found'); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!company) {
+      return res.status(404).send('Not Found');
+    }
     return res.json(company);
   });
 };
 
 // Updates an existing company in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
   Company.findById(req.params.id, function (err, company) {
-    if (err) { return handleError(res, err); }
-    if(!company) { return res.status(404).send('Not Found'); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!company) {
+      return res.status(404).send('Not Found');
+    }
     var updated = _.merge(company, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(200).json(company);
     });
   });
 };
 
 // Deletes a company from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Company.findById(req.params.id, function (err, company) {
-    if(err) { return handleError(res, err); }
-    if(!company) { return res.status(404).send('Not Found'); }
-    company.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!company) {
+      return res.status(404).send('Not Found');
+    }
+    company.remove(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(204).send('No Content');
     });
   });

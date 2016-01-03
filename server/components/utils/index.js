86633847f64116b330ce610d199aefe9e22a14d1
Matteo <mettiu@gmail.com>
2016-01-03 11:13:07 +0100

    notes

--- server/components/utils/index.js
+++ server/components/utils/index.js
@@ -29,14 +29,39 @@ exports.mongooseCreate = function (model, documentsToCreate, callback) {
   if (!documentsToCreate || !Array.isArray(documentsToCreate)) {
     return callback('first parameter should be an Array of Mongoose models')
   }
-  async.each(documentsToCreate,
-    function (document, cb) {
-      model.create(document, function (err) {
-        if (err) return cb(err);
-        cb();
-      });
-    }, function (err) {
-      if (err) return callback(err);
-      return callback();
-    });
+
+  model.create(documentsToCreate, function(err, insertedDocuments) {
+    if (err) return callback(err);
+    callback(null, insertedDocuments);
+  });
+  //async.each(documentsToCreate,
+  //  function (document, cb) {
+  //    model.create(document, function (err, created) {
+  //      if (err) return cb(err);
+  //      cb();
+  //    });
+  //  }, function (err) {
+  //    if (err) return callback(err);
+  //    return callback();
+  //  });
 };
+
+
+// CUSTOM ERROR TYPE EXAMPLE
+
+//module.exports = function CustomError(message, extra) {
+//  Error.captureStackTrace(this, this.constructor);
+//  this.name = this.constructor.name;
+//  this.message = message;
+//  this.extra = extra;
+//};
+//
+//require('util').inherits(module.exports, Error);
+//
+// ---------------------
+//
+//var CustomError = require('./errors/custom-error');
+//
+//function doSomethingBad() {
+//  throw new CustomError('It went bad!', 42);
+//}

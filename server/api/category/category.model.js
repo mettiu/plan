c10adee1ec88f0a4a76f363505affec8728f065f
Matteo <mettiu@gmail.com>
2016-01-03 17:27:49 +0100

    removed createNew method from Category model

--- server/api/category/category.model.js
+++ server/api/category/category.model.js
@@ -8,40 +8,30 @@ var CategorySchema = new Schema({
 
   name: {
     type: String,
-    //minlength:  [5, 'The value of path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length ({MINLENGTH}).'],
-    //maxlength: [50, 'The value of path `{PATH}` (`{VALUE}`) is longer than the maximum allowed length ({MAXLENGTH}).'],
-    //required: '{PATH} is required!'
+    minlength:  5,
+    maxlength: 100,
+    required: true
   },
   description: {
     type: String,
-    //maxlength: 5000
+    minlength: 0,
+    maxlength: 100
   },
   _company: {
     type: Schema.Types.ObjectId,
     ref: 'Company',
-    //required: 'is required!'
-  }
+    required: true
+  },
+  purchaseUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
 });
 
 CategorySchema.plugin(timestamps, {
   createdAt: 'createdAt',
   updatedAt: 'updatedAt'
 });
-
-
-/**
- * Statics
- */
-CategorySchema
-  .statics = {
-
-  createNew: function (model, callback) {
-    (new this(model)).save(callback);
-  }
-
-};
-
-
-
 
 module.exports = mongoose.model('Category', CategorySchema);

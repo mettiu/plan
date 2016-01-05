12a4e40e039fb0c5e6128ec0cadfbb7327a8decc
Matteo <mettiu@gmail.com>
2016-01-05 11:08:06 +0100

    routes for Category

--- server/api/category/index.js
+++ server/api/category/index.js
@@ -1,10 +1,41 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./team.controller');
-var config = require('../../config/environment');
+var controller = require('./category.controller');
 
 var router = express.Router();
+
+router.post('/',
+  auth.getTokenFromQuery,
+  auth.jwtMiddleware,
+  auth.attachUserToRequest,
+  auth.attachTargetCompanyToRequest,
+  auth.isAdminForTargetCompany,
+  controller.create);
+
+router.put('/:id',
+  auth.getTokenFromQuery,
+  auth.jwtMiddleware,
+  auth.attachUserToRequest,
+  auth.attachTargetCompanyToRequest,
+  auth.isAdminForTargetCompany,
+  controller.update);
+
+router.delete('/:id',
+  auth.getTokenFromQuery,
+  auth.jwtMiddleware,
+  auth.attachUserToRequest,
+  auth.attachTargetCompanyToRequest,
+  auth.isAdminForTargetCompany,
+  controller.destroy);
+
+router.get('/',
+  auth.getTokenFromQuery,
+  auth.jwtMiddleware,
+  auth.attachUserToRequest,
+  auth.attachTargetCompanyToRequest,
+  auth.isAdminForTargetCompany,
+  controller.index);
 
 //router.post('/issue', controller.issue);
 //router.get('/check', controller.check);
